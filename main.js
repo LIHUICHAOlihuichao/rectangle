5170733f342317212aa41d9cf7faab68fe7d5ce8
lihuichao <2686079948@qq.com>
2021-03-28 10:20:00 +0800

    v 0.2

--- main.js
+++ main.js
@@ -4,6 +4,8 @@ $(()=>{
       $height = $('#height'),
       $btnCal = $('#calc'),
       $perimeter = $('#perimeter'),
+      $widthValidate = $('#width-validate'),
+      $heightValidate = $('#height-validate'),
       $area = $('#area');
 
   //浮点运算精确度解决
@@ -17,12 +19,47 @@ $(()=>{
     let w = Number($width.val()),
         h = Number($height.val());
 
-    //calc
-    let p = roundFractional((w+h)*2,2),
-        a = roundFractional(w*h,2);
+    //validate
+    if(validate($width,$widthValidate) && validate($height,$heightValidate)){
+      //calc
+      let p = roundFractional((w+h)*2,2),
+          a = roundFractional(w*h,2);
 
-    //output
-    $perimeter.val(p);
-    $area.val(a);
+      //output
+      $perimeter.val(p);
+      $area.val(a);
+
+    }
   });
 });
+
+function validate(input,output){
+  //is empty
+  if(input.val() === ''){
+    output.html('该字段不能为空！');
+    return false;
+  } else {
+   // output.html('');
+  }
+
+  //is number
+  let val = Number(input.val());
+
+  if(isNaN(val)){
+    output.html('该字段应该是数值！');
+    return false;
+  } else{
+   // output.html('');
+  }
+   
+  // > 0
+  if(val < 0) {
+    output.html('该数值不能小于零！');
+    return false;
+  } else{
+   // output.html('');
+  }
+
+  output.html('');
+  return true;
+}
